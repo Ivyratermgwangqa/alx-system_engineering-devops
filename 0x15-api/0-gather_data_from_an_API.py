562afc33befc9d4ed0a28ffb6575216d22c9590d
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-19 22:35:16 +0200

    Update 0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Gather data from an API
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    user_id = argv[1]
+    user_url = 'https://jsonplaceholder.typicode.com/users/' + user_id
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+
+    user_response = requests.get(user_url)
+    todo_response = requests.get(todo_url)
+
+    user_data = user_response.json()
+    todo_data = todo_response.json()
+
+    employee_name = user_data.get('name')
+    total_tasks = len(todo_data)
+    done_tasks = [task for task in todo_data if task.get('completed')]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, len(done_tasks), total_tasks))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
