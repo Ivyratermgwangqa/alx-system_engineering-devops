112819da8358db44aefd75f76c32069d89999b76
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-20 01:40:46 +0200

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,12 +4,21 @@ import requests as req
 import sys
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    user_id = req.get(url + 'users/{}'.format(sys.argv[1])).json()
-    to_do = req.get(url + 'todos', params={'userId': sys.argv[1]}).json()
-    completed = [title.get("title") for title in to_do if
-                 title.get('completed') is True]
-    print("Employee {} is done with tasks({}/{}):".format(user_id.get("name"),
-                                                          len(completed),
-                                                          len(to_do)))
-    [print("\t {}".format(title)) for title in completed]
+    # Retrieve user information
+    user_id = sys.argv[1]
+    user_response = req.get(f'https://jsonplaceholder.typicode.com/users/{user_id}')
+    user_data = user_response.json()
+    employee_name = user_data.get("name")
+
+    # Retrieve user's TODO list
+    todo_response = req.get('https://jsonplaceholder.typicode.com/todos',
+                            params={'userId': user_id})
+    todo_data = todo_response.json()
+
+    # Count completed tasks
+    completed_tasks = [task for task in todo_data if task.get('completed')]
+
+    # Print employee progress
+    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{len(todo_data)}):")
+    for task in completed_tasks:
+        print(f"\t {task.get('title')}")
