8e2ddac30424d880425e53cd7fee400e2b304d39
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-20 01:50:17 +0200

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,22 @@
-#!/usr/bin/python3
 """
 Gather data from an API
 """
-
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
     user_id = argv[1]
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    user_url = 'https://jsonplaceholder.typicode.com/users/' + user_id
     todo_url = 'https://jsonplaceholder.typicode.com/todos'
 
     user_response = requests.get(user_url)
-    todo_response = requests.get(todo_url, params={'userId': user_id})
+    todo_response = requests.get(todo_url)
 
     user_data = user_response.json()
     todo_data = todo_response.json()
 
-    employee_name = user_data.get('username')  # Use 'username' instead of 'name'
+    employee_name = user_data.get('name')
     total_tasks = len(todo_data)
     done_tasks = [task for task in todo_data if task.get('completed')]
 
@@ -26,4 +24,4 @@ if __name__ == "__main__":
         employee_name, len(done_tasks), total_tasks))
 
     for task in done_tasks:
-        print("\t{}".format(task.get('title')))  # Adjust formatting for task titles
+        print("\t {}".format(task.get('title')))
