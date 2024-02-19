24474b9276c4ba63e9bd2c9cdbc18dd951a4f94a
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-20 01:22:51 +0200

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,40 @@
 #!/usr/bin/python3
-"""
-Gather data from an API
-"""
+"""Retrieve and display TODO list progress for a given employee ID."""
 
-import requests
-from sys import argv
+import requests as r
+import sys
 
+def get_todo_list(user_id):
+    """Retrieve TODO list for the given user ID."""
+    url = 'https://jsonplaceholder.typicode.com/'
+    
+    # Fetch user data
+    user_data = r.get(url + 'users/{}'.format(user_id)).json()
+    
+    # Fetch TODO list for the user
+    todo_list = r.get(url + 'todos', params={'userId': user_id}).json()
+    
+    return user_data, todo_list
 
-if __name__ == "__main__":
-    user_id = argv[1]
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    todo_url = 'https://jsonplaceholder.typicode.com/todos'
-
-    user_response = requests.get(user_url)
-    todo_response = requests.get(todo_url, params={'userId': user_id})
-
-    user_data = user_response.json()
-    todo_data = todo_response.json()
-
-    employee_name = user_data.get('username')  # Use 'username' instead of 'name'
-    total_tasks = len(todo_data)
-    done_tasks = [task for task in todo_data if task.get('completed')]
-
+def print_todo_progress(user_data, todo_list):
+    """Print TODO list progress for the given user."""
+    employee_name = user_data.get("name")
+    completed_tasks = [task for task in todo_list if task.get('completed')]
+    total_tasks = len(todo_list)
+    completed_count = len(completed_tasks)
+    
     print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, len(done_tasks), total_tasks))
+        employee_name, completed_count, total_tasks))
+    
+    # Print completed tasks
+    for title in completed_tasks:
+        print("\t{}".format(title.get("title")))
 
-    for task in done_tasks:
-        print("\t{}".format(task.get('title')))  # Adjust formatting for task titles
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+    
+    user_id = sys.argv[1]
+    user_data, todo_list = get_todo_list(user_id)
+    print_todo_progress(user_data, todo_list)
