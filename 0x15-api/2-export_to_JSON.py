2f75e7dd8daa5645cc3ec189b4847d59874b2dd8
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-20 02:58:33 +0200

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,43 +1,19 @@
 #!/usr/bin/python3
-"""
-Export data to JSON format.
-"""
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: {} employee_id".format(sys.argv[0]))
-        sys.exit(1)
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    employee_id = int(sys.argv[1])
-
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
-    todos_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)
-
-    try:
-        user_response = requests.get(user_url)
-        todos_response = requests.get(todos_url)
-        user_data = user_response.json()
-        todos_data = todos_response.json()
-    except Exception as e:
-        print("Error: {}".format(e))
-        sys.exit(1)
-
-    user_info = {
-        str(employee_id): [
-            {
-                'task': todo.get('title'),
-                'completed': todo.get('completed'),
-                'username': user_data.get('username')
-            }
-            for todo in todos_data
-        ]
-    }
-
-    filename = '{}.json'.format(employee_id)
-    with open(filename, 'w') as file:
-        json.dump(user_info, file)
-
-    print("Tasks exported to", filename)
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
