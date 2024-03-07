37c1150b8dc0f39559ab5e8938e51008316558e3
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-03-07 03:44:14 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
-"""
-1-top_ten
-"""
-import requests
+
+import requests as r
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts """
-    url = (
-        f"https://www.reddit.com/r/{subreddit}/hot.json"
-    )
-    # Set a custom User-Agent to avoid Too Many Requests error
-    headers = {"User-Agent": "MyBot/1.0"}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        for post in data["data"]["children"][:10]:
-            print(post["data"]["title"])
-    else:
-        print(None)
+    """
+    Print top 10 posts from the given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) "
+                      "Gecko/20100101 Firefox/73.0"
+    }
+    params = {"limit": 10}
+    response = r.get(url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    for top in results.get("children"):
+        print(top.get("data").get("title"))
 
 
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+    subreddit_name = input("Enter the subreddit name: ")
+    top_ten(subreddit_name)
